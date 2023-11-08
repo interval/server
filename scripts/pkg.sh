2ae17f454f4114add93d3a32b79bf6c1c0f73d07
Alex Arena <arena.secure@gmail.com>
2023-11-08 14:56:07 -0800

    adds missing deps + release script

---
+++ scripts/pkg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
+
+# yarn build
+
+rm -rf release
+mkdir release
+
+cp -r dist release/dist
+cp package.json release/package.json
+
+mkdir release/prisma
+cp prisma/schema.prisma release/prisma/schema.prisma
+cp prisma/clean-vite-cache-generator.js release/prisma/clean-vite-cache-generator.js
+
+cp -r email-templates release/email-templates
+cp -r LICENSE release/license
+
+tar -czf release.tar.gz release
