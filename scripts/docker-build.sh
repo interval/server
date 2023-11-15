24c82acd093639f904b1c0cbc0390f718ce83d96
Alex Arena <arena.secure@gmail.com>
2023-11-15 13:19:24 -0800

    also publishes to latest tag

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
-
+docker buildx create --use
 pkgV=`node -e "console.log(require('./package.json').version.trim())"`
-imageName=alexarena/interval-server:$pkgV
+specificVersionTag=alexarena/interval-server:$pkgV
 # Build the docker image + pushes it to registry
-docker buildx build --output=type=registry --platform=linux/amd64,linux/arm64 -t $imageName .
+docker buildx build --output=type=registry --platform=linux/amd64,linux/arm64 -t alexarena/interval-server:latest -t $specificVersionTag .
